9849e50a530c728881ee12170382acdd25e1f617
Ramon Torres <ramont@stripe.com>
2022-08-31 20:45:06 -0400

    Stripe SDK 22.7.1

---
+++ StripeUICore/StripeUICore/Source/Elements/Factories/DropdownFieldElement+AddressFactory.swift
@@ -0,0 +1,44 @@
+//
+//  DropdownFieldElement+AddressFactory.swift
+//  StripeUICore
+//
+//  Created by Mel Ludowise on 9/28/21.
+//
+
+import Foundation
+@_spi(STP) import StripeCore
+
+@_spi(STP) public extension DropdownFieldElement {
+    // MARK: - Address
+
+    enum Address {
+
+        // MARK: - Country
+
+        public static func makeCountry(
+            label: String,
+            countryCodes: [String],
+            theme: ElementsUITheme = .default,
+            defaultCountry: String? = nil,
+            locale: Locale = Locale.current
+        ) -> DropdownFieldElement {
+            let dropdownItems: [DropdownItem] = countryCodes.map {
+                let flagEmoji = String.countryFlagEmoji(for: $0) ?? ""              // 🇺🇸
+                let countryName = locale.localizedString(forRegionCode: $0) ?? $0   // United States
+                return DropdownItem(pickerDisplayName: "\(flagEmoji) \(countryName)",
+                                    labelDisplayName: countryName,
+                                    accessibilityLabel: countryName,
+                                    rawData: $0)
+            }
+            let defaultCountry = defaultCountry ?? locale.regionCode ?? ""
+            let defaultCountryIndex = countryCodes.firstIndex(of: defaultCountry) ?? 0
+            
+            return DropdownFieldElement(
+                items: dropdownItems,
+                defaultIndex: defaultCountryIndex,
+                label: String.Localized.country_or_region,
+                theme: theme
+            )
+        }
+    }
+}
