9c8848401a3a1d111936e89e021439e1de9301f9
Ramon Torres <ramont@stripe.com>
2022-10-31 16:40:19 -0400

    Stripe SDK 23.1.0

---
+++ StripeIdentity/StripeIdentity/Source/API Bindings/Models/VerificationPageDataUpdate/VerificationPageClearData.swift
@@ -0,0 +1,32 @@
+//
+//  VerificationPageClearData.swift
+//  StripeIdentity
+//
+//  Created by Mel Ludowise on 3/2/22.
+//  Copyright © 2022 Stripe, Inc. All rights reserved.
+//
+
+import Foundation
+@_spi(STP) import StripeCore
+
+extension StripeAPI {
+    struct VerificationPageClearData: Encodable, Equatable {
+        let biometricConsent: Bool?
+        let face: Bool?
+        let idDocumentBack: Bool?
+        let idDocumentFront: Bool?
+        let idDocumentType: Bool?
+    }
+}
+
+extension StripeAPI.VerificationPageClearData {
+    init(clearFields fields: Set<StripeAPI.VerificationPageFieldType>) {
+        self.init(
+            biometricConsent: fields.contains(.biometricConsent),
+            face: fields.contains(.face),
+            idDocumentBack: fields.contains(.idDocumentBack),
+            idDocumentFront: fields.contains(.idDocumentFront),
+            idDocumentType: fields.contains(.idDocumentType)
+        )
+    }
+}
---
+++ StripeIdentity/StripeIdentity/Source/NativeComponents/Detectors/FaceDetector/FaceDetector.swift
@@ -0,0 +1,53 @@
+//
+//  FaceDetector.swift
+//  StripeIdentity
+//
+//  Created by Mel Ludowise on 5/10/22.
+//  Copyright © 2022 Stripe, Inc. All rights reserved.
+//
+
+import Foundation
+import CoreVideo
+import Vision
+@_spi(STP) import StripeCameraCore
+@_spi(STP) import StripeCore
+
+/**
+ Scans an image using the FaceDetector ML model.
+ */
+
+final class FaceDetector: VisionBasedDetector {
+    typealias Output = FaceDetectorOutput
+
+    typealias Configuration = MLDetectorConfiguration
+
+    let model: VNCoreMLModel
+    let configuration: Configuration
+    let metricsTracker: MLDetectorMetricsTracker? = .init(modelName: "face_detector_v1")
+
+    /**
+     Initializes an `FaceDetector`
+     - Parameters:
+       - model: The FaceDetector ML model loaded into a `VNCoreMLModel`
+       - configuration: The configuration for this detector
+     */
+    init(
+        model: VNCoreMLModel,
+        configuration: Configuration
+    ) {
+        self.model = model
+        self.configuration = configuration
+    }
+
+    func visionBasedDetectorMakeRequest() -> VNImageBasedRequest {
+        let request = VNCoreMLRequest(model: model)
+        // The FaceDetector model requires a square region as input, so configure
+        // the request to only consider the center-cropped region.
+        request.imageCropAndScaleOption = .centerCrop
+        return request
+    }
+
+    func visionBasedDetectorOutputIfSkipping() -> FaceDetectorOutput? {
+        return .none
+    }
+}
